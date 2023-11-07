2aac0a133cb883366c7c7e8e3670b7ba759c977d
joregesosa <joregesosa@gmail.com>
2023-11-07 16:49:12 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,57 @@
+const usuarios = [
+    {
+        nombre: "Juan Pérez",
+        rol: "maestro",
+        isActive: true
+    },
+    {
+        nombre: "María García",
+        rol: "administrador",
+        isActive: false
+    },
+    {
+        nombre: "Pedro Sánchez",
+        rol: "alumno",
+        isActive: true
+    },
+    {
+        nombre: "Ana Rodríguez",
+        rol: "maestro",
+        isActive: false
+    },
+    {
+        nombre: "Carlos López",
+        rol: "administrador",
+        isActive: true
+    },
+    {
+        nombre: "Sofía Martínez",
+        rol: "alumno",
+        isActive: false
+    },
+    {
+        nombre: "Laura Torres",
+        rol: "maestro",
+        isActive: true
+    },
+    {
+        nombre: "David González",
+        rol: "administrador",
+        isActive: false
+    },
+    {
+        nombre: "Isabel Castro",
+        rol: "alumno",
+        isActive: true
+    },
+    {
+        nombre: "Marcos Rivera",
+        rol: "maestro",
+        isActive: false
+    }
+];
+
+const ADM = ['Usuarios', 'Calificaciones', 'Alumnos', 'Blogs', 'Materias'];
+const MAESTRO = ['Calificaciones', 'Alumnos', 'Blogs', 'Materias'];
+const ALUMNOS = ['Calificaciones', 'Blogs', 'Materias'];
+
